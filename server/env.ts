c079676f6220d8c259ae2fad1aebe8438b0d8cd6
Bryan-Galaxy-WSL-Ubuntu <eobryandev@gmail.com>
2025-08-17 18:05:52 -0300

    feat: creating jwt service

--- server/env.ts
+++ server/env.ts
@@ -2,7 +2,9 @@ import { z } from 'zod';
 
 const envSchema = z.object({
   SERVER_PORT: z.coerce.number().default(3333),
-  POSTGRES_URL: z.url().startsWith('postgresql://')
+  POSTGRES_URL: z.string().startsWith('postgresql://').default(''),
+  JWT_SECRET: z.coerce.string(),
+  JWT_EXPIRES_INT: z.coerce.number().default(60)
 });
 
 export const env = envSchema.parse(process.env);
---
+++ server/src/infrastructure/dto/IDataInToken.ts
@@ -0,0 +1,8 @@
+import { TRole } from "../../domain/entities/interfaces/TRole"
+
+interface IDataInToken {
+    role: TRole,
+    user_id: string
+}
+
+export { IDataInToken }
