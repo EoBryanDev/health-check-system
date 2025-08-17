e22d40c050cf69deec93886afce420e181bf2523
Bryan-Galaxy-WSL-Ubuntu <eobryandev@gmail.com>
2025-08-17 19:37:50 -0300

    feat: creating create group use case

---
+++ server/src/domain/use_cases/createMonitGroupUseCase.ts
@@ -0,0 +1,12 @@
+import { IRepository } from "../entities/interfaces/IRepository";
+
+class CreateMonitGroupUseCase {
+    constructor(private inMemoryRepository: IRepository) { }
+
+    public async execute() {
+
+    }
+
+}
+
+export { CreateMonitGroupUseCase }
