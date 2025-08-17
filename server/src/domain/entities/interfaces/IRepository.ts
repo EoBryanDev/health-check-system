e1e2f593ebaf0e7f4fa8694377706bfca853e5e1
Bryan-Galaxy-WSL-Ubuntu <eobryandev@gmail.com>
2025-08-17 19:39:01 -0300

    chore: creating group repository

--- server/src/domain/entities/interfaces/IRepository.ts
+++ server/src/domain/entities/interfaces/IRepository.ts
@@ -1,3 +1,4 @@
+import { IGroupOutputDTO, IGroupOutputUsersDTO } from "../../../infrastructure/dto/IGroupDTO";
 
 interface IRepository {
     findUserByEmail(email: string): Promise<any | null>;
@@ -11,6 +12,18 @@ interface IRepository {
     // delete(id: string): Promise<void>;
 
     findAllUsers(): Promise<any[]>;
+
+    createGroup(): Promise<void>;
+
+    findAllGroups(): Promise<IGroupOutputDTO[] | null>;
+
+    findGroupById(group_id: string): Promise<IGroupOutputDTO | null>;
+
+    findGroupMembersById(user_id: string): Promise<IGroupOutputUsersDTO[] | null>;
+
+    // inativeGroup(group_id: string): Promise<void>;
+
+
 }
 
 export { IRepository };
--- server/src/infrastructure/repository/InMemoryRepository.ts
+++ server/src/infrastructure/repository/InMemoryRepository.ts
@@ -1,9 +1,11 @@
 import { IRepository } from "../../domain/entities/interfaces/IRepository";
+import { IGroupOutputDTO, IGroupOutputUsersDTO } from "../dto/IGroupDTO";
 
 
 class InMemoryRepository implements IRepository {
 
     readonly users: any[] = [];
+    readonly groups: any[] = [];
 
     async findUserByEmail(email: string): Promise<any | null> {
 
@@ -25,10 +27,24 @@ class InMemoryRepository implements IRepository {
     // }
 
     async findAllUsers(): Promise<any[]> {
-
         return [];
     }
 
+    async createGroup(): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    async findAllGroups(): Promise<IGroupOutputDTO[] | null> {
+        throw new Error("Method not implemented.");
+    }
+    async findGroupById(group_id: string): Promise<IGroupOutputDTO | null> {
+        throw new Error("Method not implemented.");
+    }
+    async findGroupMembersById(user_id: string): Promise<IGroupOutputUsersDTO[] | null> {
+        throw new Error("Method not implemented.");
+    }
+    // async inativeGroup(group_id: string): Promise<void> {
+    //     throw new Error("Method not implemented.");
+    // }
 }
 
 export { InMemoryRepository };
