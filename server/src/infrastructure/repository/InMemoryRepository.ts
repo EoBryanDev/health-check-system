24f049c654ebacfa1d588b00112c9efbe040c5b8
Bryan-Galaxy-WSL-Ubuntu <eobryandev@gmail.com>
2025-08-17 19:12:55 -0300

    fix: Recovering user development

--- server/src/infrastructure/repository/InMemoryRepository.ts
+++ server/src/infrastructure/repository/InMemoryRepository.ts
@@ -1,18 +1,76 @@
-import { IRepository } from "../../domain/entities/interfaces/IRepository";
+import { IUser } from "../../domain/entities/interfaces/IUser";
+import { IUserRepository } from "../../domain/entities/interfaces/IUserRepository";
+import { User } from "../../domain/entities/User";
+import { ICreateUserOutputDTO, ICreateUserOutputWPwdDTO } from "../dto/ICreateUserDTO";
 
 
-class InMemoryRepository implements IRepository {
+class InMemoryRepository implements IUserRepository {
 
-    readonly users: any[] = [];
+    readonly users: User[] = [];
 
-    async findUserByEmail(email: string): Promise<any | null> {
+    async findByEmail(email: string): Promise<ICreateUserOutputWPwdDTO | null> {
 
-        return null;
+        const user = this.users.find(user => user.getUserInfo().email! === email) || null
+
+        if (!user) {
+            return null;
+        }
+
+        const user_info = user.getUserInfo()
+
+        const user_dto: ICreateUserOutputWPwdDTO = {
+            user_id: user_info.user_id!,
+            first_name: user_info.first_name!,
+            last_name: user_info.last_name!,
+            email: user_info.email!,
+            created_at: user_info.created_at!,
+            role: user_info.role!,
+            password: user_info.password!
+        }
+        return user_dto;
     }
 
-    async createUser(user: any): Promise<any> {
+    // async findById(id: string): Promise<User | null> {
+    //     return this.users.find(user => user.props.id === id) || null;
+    // }
 
-        return null;
+    // async update(user: User): Promise<void> {
+    //     const index = this.users.findIndex(u => u.getUserInfo().user_id! === user.getUserInfo().user_id!);
+    //     if (index !== -1) {
+    //         this.users[index] = user;
+    //     }
+    // }
+
+    async create(user: User): Promise<ICreateUserOutputDTO> {
+
+        const last_user = this.users.findLast((user) => user.getUserInfo().user_id !== undefined);
+        const new_id = last_user ? last_user.getUserInfo().user_id + 'a' : 'a'
+
+        const payload_user = user.getUserInfo()
+
+        const new_user_payload: IUser = {
+            user_id: new_id,
+            first_name: payload_user.first_name,
+            last_name: payload_user.last_name,
+            email: payload_user.email,
+            password: payload_user.password,
+            cellnumber: payload_user.cellnumber
+        }
+
+        const createdUser = new User(new_user_payload)
+
+        this.users.push(createdUser);
+
+        const user_info = createdUser.getUserInfo()
+
+        const user_dto: ICreateUserOutputDTO = {
+            user_id: user_info.user_id!,
+            first_name: user_info.first_name!,
+            last_name: user_info.last_name!,
+            email: user_info.email!,
+            created_at: user_info.created_at!,
+        }
+        return user_dto;
     }
 
     // async delete(id: string): Promise<void> {
@@ -24,9 +82,21 @@ class InMemoryRepository implements IRepository {
 
     // }
 
-    async findAllUsers(): Promise<any[]> {
+    async findAll(): Promise<ICreateUserOutputDTO[]> {
 
-        return [];
+        const all_users: ICreateUserOutputDTO[] = this.users.map((user) => {
+            const user_info = user.getUserInfo();
+
+            return {
+                user_id: user_info.user_id!,
+                first_name: user_info.first_name!,
+                last_name: user_info.last_name!,
+                email: user_info.email!,
+                created_at: user_info.created_at!,
+            }
+        })
+
+        return all_users;
     }
 
 }
