f767f9ac0824ec07ea00df5afee47cb4d021d51f
Bryan-Galaxy-WSL-Ubuntu <eobryandev@gmail.com>
2025-08-17 16:12:26 -0300

    feat: Creating create user use case implementation

--- server/src/domain/use_cases/CreateUserUseCase.ts
+++ server/src/domain/use_cases/CreateUserUseCase.ts
@@ -1,14 +1,25 @@
-import { CreateUserInputDTO, CreateUserOutputDTO } from "../../infrastructure/dto/CreateUserDTO";
+
+import { ICreateUserInputDTO, ICreateUserOutputDTO } from "../../infrastructure/dto/ICreateUserDTO";
 import { IUserRepository } from "../entities/interfaces/IUserRepository";
 import { User } from "../entities/User";
 
 class CreateUserUseCase {
     constructor(private userRepository: IUserRepository) { }
 
-    // async execute(user_payload: CreateUserInputDTO): Promise<CreateUserOutputDTO> {
+    async execute(user_payload: ICreateUserInputDTO): Promise<ICreateUserOutputDTO> {
+        const user = await User.create(user_payload);
+        const user_info = user.getUserInfo();
 
-    //     return
-    // }
+        const already_exist_user = await this.userRepository.findByEmail(user_info.email);
+
+        if (already_exist_user) {
+            throw new Error("User email already registered!");
+        }
+
+        const response = await this.userRepository.create(user)
+
+        return response
+    }
 }
 
 export { CreateUserUseCase }
