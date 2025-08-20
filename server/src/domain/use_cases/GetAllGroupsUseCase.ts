b1ba12240bba022e7b62840cc57786b020a74ae8
Bryan-Galaxy-WSL-Ubuntu <eobryandev@gmail.com>
2025-08-20 20:47:10 -0300

    fix: adapting consult to repository update

--- server/src/domain/use_cases/GetAllGroupsUseCase.ts
+++ server/src/domain/use_cases/GetAllGroupsUseCase.ts
@@ -3,17 +3,18 @@ import { IGroupInputDTO, IGroupOutputDTO } from "../../infrastructure/dto/IGroup
 import { IMonitGroup } from "../entities/interfaces/IMonitGroup";
 import { IRepository } from "../entities/interfaces/IRepository";
 import { MonitGroup } from "../entities/MonitGroup";
+import { IQueryParams } from "./interfaces/IQueryParams";
 
 class GetAllGroupsUseCase {
     constructor(private inMemoryRepository: IRepository) { }
 
-    public async execute(data_in_token: IDataInToken): Promise<IGroupOutputDTO[]> {
+    public async execute(data_in_token: IDataInToken, params: IQueryParams): Promise<IGroupOutputDTO[]> {
         if (data_in_token.role === 'ANALYST') {
             throw new Error("The current user does not have privileges to do this action!");
 
         }
 
-        const response = await this.inMemoryRepository.findAllGroups()
+        const response = await this.inMemoryRepository.findAllGroups(params)
 
         if (!response) {
             throw new Error("There was not found any group!");
