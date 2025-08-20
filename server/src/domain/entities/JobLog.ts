622401b1e4d5d4708f1a9b5f56020a0d6172ca40
Bryan-Galaxy-WSL-Ubuntu <eobryandev@gmail.com>
2025-08-20 19:36:52 -0300

    feat: creating job and service log entity

---
+++ server/src/domain/entities/JobLog.ts
@@ -0,0 +1,24 @@
+import { IJobLog } from "./interfaces/IJobLog";
+
+class JobLog {
+
+    private props: IJobLog;
+
+    constructor(job_log_payload: IJobLog) {
+        const job_log_constructor: IJobLog = {
+            job_log_id: job_log_payload.job_log_id ?? undefined,
+            job_id: job_log_payload.job_id,
+            start_at: job_log_payload.start_at,
+            duration: job_log_payload.duration,
+        }
+
+        this.props = job_log_constructor
+
+    }
+
+    public getJobLogInfo() {
+        return this.props
+    }
+}
+
+export { JobLog }
