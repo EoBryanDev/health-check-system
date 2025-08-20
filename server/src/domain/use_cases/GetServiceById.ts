79e8e6123c31789cbd6f1f768857628129ffccf2
Bryan-Galaxy-WSL-Ubuntu <eobryandev@gmail.com>
2025-08-20 19:01:24 -0300

    feat: creating get service by id use case

---
+++ server/src/domain/use_cases/GetServiceById.ts
@@ -0,0 +1,19 @@
+import { IDataInToken } from "../../infrastructure/dto/IDataInToken";
+import { IRepository } from "../entities/interfaces/IRepository";
+
+class GetServiceById {
+    constructor(private dbRepository: IRepository) { }
+
+    async execute(service_id: string) {
+
+        const response = await this.dbRepository.findServiceById(service_id);
+
+        if (!response) {
+            throw new Error("There was not found any service with the id informed.");
+        }
+
+        return response;
+    }
+}
+
+export { GetServiceById }
