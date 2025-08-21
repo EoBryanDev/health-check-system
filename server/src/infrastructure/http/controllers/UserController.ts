0ecfeb7a6a5ed7f0d2ec65cc2f016508f65c207a
Bryan-Galaxy-WSL-Ubuntu <eobryandev@gmail.com>
2025-08-21 19:46:47 -0300

    feat: creating user controller and route

---
+++ server/src/infrastructure/http/controllers/UserController.ts
@@ -0,0 +1,34 @@
+import { CreateUserUseCase } from "../../../domain/use_cases/CreateUserUseCase";
+import { Request, Response } from "express";
+import { IHTTPSuccessOutputDTO } from "../../dto/IHTTPOutputDTO";
+import { createUserSchema } from "../zodSchemas/user.post";
+
+class UserController {
+    constructor(private createUserUseCase: CreateUserUseCase) { }
+
+    async createUser(req: Request, resp: Response) {
+
+        const { body } = req
+
+        const valid_body = createUserSchema.parse(body)
+
+        const response = await this.createUserUseCase.execute(valid_body)
+
+        const outputSuccessDTO: IHTTPSuccessOutputDTO = {
+            data: response
+        }
+
+        resp.status(200).json(outputSuccessDTO)
+    }
+
+    async findUser(req: Request, resp: Response) {
+
+
+    }
+
+    async findAllUsers(req: Request, resp: Response) {
+
+    }
+}
+
+export { UserController }
