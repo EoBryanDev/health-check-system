1251eca01dc9622f5b4fc1da215a19d60ffde5d8
Bryan-Galaxy-WSL-Ubuntu <eobryandev@gmail.com>
2025-08-21 21:25:05 -0300

    fix: adjusting monito group use case, receiving a only group

--- server/src/domain/use_cases/CreateMonitGroupUseCase.ts
+++ server/src/domain/use_cases/CreateMonitGroupUseCase.ts
@@ -7,7 +7,7 @@ import { MonitGroup } from "../entities/MonitGroup";
 class CreateMonitGroupUseCase {
     constructor(private inMemoryRepository: IRepository) { }
 
-    public async execute(payload_groups: IGroupInputDTO[], data_in_token: IDataInToken) {
+    public async execute(payload_group: IGroupInputDTO, data_in_token: IDataInToken) {
         if (data_in_token.role === 'ANALYST') {
             throw new Error("The current user does not have privileges to do this action!");
 
@@ -15,20 +15,18 @@ class CreateMonitGroupUseCase {
 
         const users_created: IMonitGroup[] = []
 
-        for (let i = 0; i < payload_groups.length; i++) {
-            const payload_group = payload_groups[i];
 
-            const alreadyExistGroup = await this.inMemoryRepository.findGroupByName(payload_group.group_name)
-            if (alreadyExistGroup) {
-                throw new Error("Group name is already registered");
 
-            }
-
-            const response = await this.inMemoryRepository.createGroup(payload_group, data_in_token.user_id)
-
-            users_created.push(response)
+        const alreadyExistGroup = await this.inMemoryRepository.findGroupByName(payload_group.group_name)
+        if (alreadyExistGroup) {
+            throw new Error("Group name is already registered");
 
         }
+
+        const response = await this.inMemoryRepository.createGroup(payload_group, data_in_token.user_id)
+
+        users_created.push(response)
+
 
 
         return users_created
