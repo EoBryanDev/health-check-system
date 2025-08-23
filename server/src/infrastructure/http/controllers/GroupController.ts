dc5f22921c59317206275c71f5f3111b8f8b8548
Bryan-Galaxy-WSL-Ubuntu <eobryandev@gmail.com>
2025-08-23 13:21:16 -0300

    chore: implemeting eslint e prettier and running

--- server/src/infrastructure/http/controllers/GroupController.ts
+++ server/src/infrastructure/http/controllers/GroupController.ts
@@ -1,74 +1,79 @@
-
-import { Request, Response } from "express";
-import { IHTTPErrorOutputDTO, IHTTPSuccessOutputDTO } from "../../dto/IHTTPOutputDTO";
-import { CreateMonitGroupUseCase } from "../../../domain/use_cases/CreateMonitGroupUseCase";
-import { createGroupSchema } from "../zodSchemas/group.post";
-import { GetAllGroupsUseCase } from "../../../domain/use_cases/GetAllGroupsUseCase";
-import { IQueryParams } from "../../../domain/use_cases/interfaces/IQueryParams";
+import { Request, Response } from 'express';
+import {
+  IHTTPErrorOutputDTO,
+  IHTTPSuccessOutputDTO,
+} from '../../dto/IHTTPOutputDTO';
+import { CreateMonitGroupUseCase } from '../../../domain/use_cases/CreateMonitGroupUseCase';
+import { createGroupSchema } from '../zodSchemas/group.post';
+import { GetAllGroupsUseCase } from '../../../domain/use_cases/GetAllGroupsUseCase';
+import { IQueryParams } from '../../../domain/use_cases/interfaces/IQueryParams';
 
 class GroupController {
-    constructor(private createMonitGroupUseCase: CreateMonitGroupUseCase, private getAllGroupsUseCase: GetAllGroupsUseCase) { }
+  constructor(
+    private createMonitGroupUseCase: CreateMonitGroupUseCase,
+    private getAllGroupsUseCase: GetAllGroupsUseCase
+  ) {}
 
-    async createGroup(req: Request, resp: Response) {
-        try {
+  async createGroup(req: Request, resp: Response) {
+    try {
+      const { body } = req;
+      const { user_id, role } = req.user!;
 
-            const { body } = req
-            const { user_id, role } = req.user!
+      const valid_body = createGroupSchema.parse(body);
 
-            const valid_body = createGroupSchema.parse(body)
+      const response = await this.createMonitGroupUseCase.execute(valid_body, {
+        user_id,
+        role,
+      });
 
-            const response = await this.createMonitGroupUseCase.execute(valid_body, { user_id, role })
+      const outputSuccessDTO: IHTTPSuccessOutputDTO = {
+        data: response,
+      };
 
-            const outputSuccessDTO: IHTTPSuccessOutputDTO = {
-                data: response
-            }
-
-            resp.status(200).json(outputSuccessDTO)
-        } catch (error) {
-            if (error instanceof Error) {
-                const resp_error: IHTTPErrorOutputDTO = {
-                    error: error.message
-                }
-                resp.status(400).json(resp_error)
-
-            } else {
-
-                resp.status(500).json(error)
-            }
-        }
+      resp.status(200).json(outputSuccessDTO);
+    } catch (error) {
+      if (error instanceof Error) {
+        const resp_error: IHTTPErrorOutputDTO = {
+          error: error.message,
+        };
+        resp.status(400).json(resp_error);
+      } else {
+        resp.status(500).json(error);
+      }
     }
+  }
 
-    async findAllGroups(req: Request, resp: Response) {
-        try {
+  async findAllGroups(req: Request, resp: Response) {
+    try {
+      const { user_id, role } = req.user!;
 
-            const { user_id, role } = req.user!
+      const params: IQueryParams = {
+        active: undefined,
+        offset: 0, // non implemented yey
+        limit: 0, // non implemented yey
+      }; // in the future i'll get it from req.params
 
-            const params: IQueryParams = {
-                active: undefined,
-                offset: 0, // non implemented yey
-                limit: 0 // non implemented yey
-            } // in the future i'll get it from req.params 
+      const response = await this.getAllGroupsUseCase.execute(
+        { user_id, role },
+        params
+      );
 
-            const response = await this.getAllGroupsUseCase.execute({ user_id, role }, params)
+      const outputSuccessDTO: IHTTPSuccessOutputDTO = {
+        data: response,
+      };
 
-            const outputSuccessDTO: IHTTPSuccessOutputDTO = {
-                data: response
-            }
-
-            resp.status(200).json(outputSuccessDTO)
-        } catch (error) {
-            if (error instanceof Error) {
-                const resp_error: IHTTPErrorOutputDTO = {
-                    error: error.message
-                }
-                resp.status(400).json(resp_error)
-
-            } else {
-
-                resp.status(500).json(error)
-            }
-        }
+      resp.status(200).json(outputSuccessDTO);
+    } catch (error) {
+      if (error instanceof Error) {
+        const resp_error: IHTTPErrorOutputDTO = {
+          error: error.message,
+        };
+        resp.status(400).json(resp_error);
+      } else {
+        resp.status(500).json(error);
+      }
     }
+  }
 }
 
 // async findUser(req: Request, resp: Response) {
@@ -80,7 +85,7 @@ class GroupController {
 //         data: response
 //     }
 
-//     resp.status(200).json(outputSuccessDTO) 
+//     resp.status(200).json(outputSuccessDTO)
 
 // }
 
@@ -93,5 +98,4 @@ class GroupController {
 //     resp.status(200).json(outputSuccessDTO)
 // }
 
-
-export { GroupController }
+export { GroupController };
--- server/src/infrastructure/http/routes/private_routes/group/group.route.ts
+++ server/src/infrastructure/http/routes/private_routes/group/group.route.ts
@@ -1,25 +1,28 @@
-import { Router } from "express";
-import { DrizzlePostgreRepository } from "../../../../repository/DrizzlePostgreRepository";
-import { BCryptHashPwd } from "../../../../../domain/services/BCryptHashPwd";
-import { JwtTokenGenerator } from "../../../../../domain/services/JwtTokenGenerator";
-import { RouteFactory } from "../../RouteFactory";
-import { authMiddleware } from "../../../../middlewares/authHandler";
+import { Router, Request, Response } from 'express';
+import { DrizzlePostgreRepository } from '../../../../repository/DrizzlePostgreRepository';
+import { BCryptHashPwd } from '../../../../../domain/services/BCryptHashPwd';
+import { JwtTokenGenerator } from '../../../../../domain/services/JwtTokenGenerator';
+import { RouteFactory } from '../../RouteFactory';
+import { authMiddleware } from '../../../../middlewares/authHandler';
 
 const db = new DrizzlePostgreRepository();
 const hash = new BCryptHashPwd();
 const token = new JwtTokenGenerator();
 
-const factory = RouteFactory.getInstance(db, hash, token)
+const factory = RouteFactory.getInstance(db, hash, token);
 
-const group = Router()
+const group: Router = Router();
 
 const groupController = factory.getGroupControllerInstance();
 
-group.post('/groups', authMiddleware, (req, res) => groupController.createGroup(req, res))
+group.post('/groups', authMiddleware, (req: Request, res: Response) =>
+  groupController.createGroup(req, res)
+);
 
-// group.get('/groups/:id', (req, res) => groupController.findgroup(req, res))
+// group.get('/groups/:id', (req: Request, res: Response) => groupController.findgroup(req, res))
 
-group.get('/groups', authMiddleware, (req, res) => groupController.findAllGroups(req, res))
+group.get('/groups', authMiddleware, (req: Request, res: Response) =>
+  groupController.findAllGroups(req, res)
+);
 
-
-export { group }
+export { group };
