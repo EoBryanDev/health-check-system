1432343eaa553774937c358dfd609f5d8a228fd4
Bryan-Galaxy-WSL-Ubuntu <eobryandev@gmail.com>
2025-08-23 13:22:18 -0300

    chore: implemeting eslint e prettier and running

--- server/src/domain/use_cases/GetAllJobsUseCase.ts
+++ server/src/domain/use_cases/GetAllJobsUseCase.ts
@@ -1,28 +1,23 @@
-import { IDataInToken } from "../../infrastructure/dto/IDataInToken";
-import { IJobInputDTO } from "../../infrastructure/dto/IJobDTO";
-import { IRepository } from "../entities/interfaces/IRepository";
-import { IQueryParams } from "./interfaces/IQueryParams";
+import { IDataInToken } from '../../infrastructure/dto/IDataInToken';
+import { IRepository } from '../entities/interfaces/IRepository';
+import { IQueryParams } from './interfaces/IQueryParams';
 
 class GetAllJobsUseCase {
-    constructor(private repository: IRepository) { }
+  constructor(private repository: IRepository) {}
 
-    async execute(data_in_token: IDataInToken, params: IQueryParams) {
-
-        if (data_in_token.role === 'ANALYST') {
-            throw new Error("");
-        }
-
-        const response = await this.repository.findAllJobs(params)
-
-        if (!response) {
-            throw new Error("There was not found any job registered!");
-
-        }
-
-        return response;
-
-
+  async execute(data_in_token: IDataInToken, params: IQueryParams) {
+    if (data_in_token.role === 'ANALYST') {
+      throw new Error('');
     }
+
+    const response = await this.repository.findAllJobs(params);
+
+    if (!response) {
+      throw new Error('There was not found any job registered!');
+    }
+
+    return response;
+  }
 }
 
-export { GetAllJobsUseCase }
+export { GetAllJobsUseCase };
--- server/src/infrastructure/http/routes/private_routes/job/job.route.ts
+++ server/src/infrastructure/http/routes/private_routes/job/job.route.ts
@@ -1,30 +1,36 @@
-import { Router } from "express";
-import { DrizzlePostgreRepository } from "../../../../repository/DrizzlePostgreRepository";
-import { BCryptHashPwd } from "../../../../../domain/services/BCryptHashPwd";
-import { JwtTokenGenerator } from "../../../../../domain/services/JwtTokenGenerator";
-import { RouteFactory } from "../../RouteFactory";
-import { authMiddleware } from "../../../../middlewares/authHandler";
-import { RedisCacheProvider } from "../../../../../domain/services/RedisCacheService";
+import { Router, Request, Response } from 'express';
+import { DrizzlePostgreRepository } from '../../../../repository/DrizzlePostgreRepository';
+import { BCryptHashPwd } from '../../../../../domain/services/BCryptHashPwd';
+import { JwtTokenGenerator } from '../../../../../domain/services/JwtTokenGenerator';
+import { RouteFactory } from '../../RouteFactory';
+import { authMiddleware } from '../../../../middlewares/authHandler';
 
 const db = new DrizzlePostgreRepository();
 const hash = new BCryptHashPwd();
 const token = new JwtTokenGenerator();
 
-const factory = RouteFactory.getInstance(db, hash, token)
+const factory = RouteFactory.getInstance(db, hash, token);
 
-const job = Router()
+const job: Router = Router();
 
 const jobController = factory.getJobControllerInstance();
 
-job.post('/jobs', authMiddleware, (req, res) => jobController.createJob(req, res))
+job.post('/jobs', authMiddleware, (req: Request, res: Response) =>
+  jobController.createJob(req, res)
+);
 
-// job.get('/jobs/:id', (req, res) => jobController.findjob(req, res))
+// job.get('/jobs/:id', (req: Request, res: Response) => jobController.findjob(req, res))
 
-job.get('/jobs', authMiddleware, (req, res) => jobController.findAll(req, res))
+job.get('/jobs', authMiddleware, (req: Request, res: Response) =>
+  jobController.findAll(req, res)
+);
 
-job.post('/jobs/service', authMiddleware, (req, res) => jobController.addServiceToJob(req, res))
+job.post('/jobs/service', authMiddleware, (req: Request, res: Response) =>
+  jobController.addServiceToJob(req, res)
+);
 
-job.put('/jobs/run/:id', authMiddleware, (req, res) => jobController.runJob(req, res))
+job.put('/jobs/run/:id', authMiddleware, (req: Request, res: Response) =>
+  jobController.runJob(req, res)
+);
 
-
-export { job }
+export { job };
