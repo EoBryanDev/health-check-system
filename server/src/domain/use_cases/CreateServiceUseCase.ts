709192b8d2da4ac7f48f3f586e108c1a5f8c18ea
Bryan-Galaxy-WSL-Ubuntu <eobryandev@gmail.com>
2025-08-23 13:23:29 -0300

    chore: implemeting eslint e prettier and running

--- server/src/domain/use_cases/CreateServiceUseCase.ts
+++ server/src/domain/use_cases/CreateServiceUseCase.ts
@@ -1,25 +1,33 @@
-import { IDataInToken } from "../../infrastructure/dto/IDataInToken";
-import { IServiceInputDTO } from "../../infrastructure/dto/IServiceDTO";
-import { IRepository } from "../entities/interfaces/IRepository";
+import { IDataInToken } from '../../infrastructure/dto/IDataInToken';
+import { IServiceInputDTO } from '../../infrastructure/dto/IServiceDTO';
+import { IRepository } from '../entities/interfaces/IRepository';
 
 class CreateServiceUseCase {
-    constructor(private dbRepository: IRepository) { }
+  constructor(private dbRepository: IRepository) {}
 
-    async execute(service_payload: IServiceInputDTO, data_in_token: IDataInToken) {
-        if (data_in_token.role === 'ANALYST') {
-            throw new Error("This user does not have permission to this!");
-        }
-
-        const alreadyExistService = await this.dbRepository.findServiceByName(service_payload.service_name);
-
-        if (alreadyExistService) {
-            throw new Error("Already exist a service with the same name.");
-        }
-
-        const response = await this.dbRepository.createService(service_payload, data_in_token.user_id);
-
-        return response;
+  async execute(
+    service_payload: IServiceInputDTO,
+    data_in_token: IDataInToken
+  ) {
+    if (data_in_token.role === 'ANALYST') {
+      throw new Error('This user does not have permission to this!');
     }
+
+    const alreadyExistService = await this.dbRepository.findServiceByName(
+      service_payload.service_name
+    );
+
+    if (alreadyExistService) {
+      throw new Error('Already exist a service with the same name.');
+    }
+
+    const response = await this.dbRepository.createService(
+      service_payload,
+      data_in_token.user_id
+    );
+
+    return response;
+  }
 }
 
-export { CreateServiceUseCase }
+export { CreateServiceUseCase };
--- server/src/infrastructure/http/zodSchemas/service.post.schema.ts
+++ server/src/infrastructure/http/zodSchemas/service.post.schema.ts
@@ -1,15 +1,15 @@
-import { z } from 'zod'
-
+import { z } from 'zod';
 
 export const createServiceSchema = z.object({
-    group_id: z.string(),
-    group_name: z.string(),
-    job_id: z.string().optional(),
-    job_name: z.string().optional(),
-    service_name: z.string(),
-    service_description: z.string().optional(),
-    service_url: z.string(),
-    rate_limit_tolerance: z.number(),
-})
+  group_id: z.string(),
+  group_name: z.string(),
+  job_id: z.string().optional(),
+  job_name: z.string().optional(),
+  last_run: z.date().optional(),
+  service_name: z.string(),
+  service_description: z.string().optional(),
+  service_url: z.string(),
+  rate_limit_tolerance: z.number(),
+});
 
 export type CreateServiceSchema = z.infer<typeof createServiceSchema>;
