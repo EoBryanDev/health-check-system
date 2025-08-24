7bcc7808a4339594f213950ae12cae3bc7ce89ef
Bryan-Galaxy-WSL-Ubuntu <eobryandev@gmail.com>
2025-08-24 10:22:15 -0300

    feat: implementing application use route

--- front/src/middleware.ts
+++ front/src/middleware.ts
@@ -1,66 +1,43 @@
-import { type NextRequest, type MiddlewareConfig, NextResponse } from 'next/server'
+import { type NextRequest, type MiddlewareConfig, NextResponse } from 'next/server';
 
-export const TOKEN_KEY = 'token'
+export const TOKEN_KEY = 'token';
 const publicRoutes: { path: string | RegExp; whenAuthenticated: 'redirect' | 'next' }[] = [
-    { path: '/sign-in', whenAuthenticated: 'redirect' },
-    { path: '/', whenAuthenticated: 'next' },
-    { path: '/sign-up', whenAuthenticated: 'next' }
-
-
+    { path: '/', whenAuthenticated: 'redirect' },
+    { path: '/sign-up', whenAuthenticated: 'redirect' },
 ] as const;
 
-const REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE = '/'
+const REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE = '/';
+const REDIRECT_WHEN_AUTHENTICATED_ROUTE = '/dashboard';
 
 export async function middleware(request: NextRequest) {
-    const path = request.nextUrl.pathname
+    const path = request.nextUrl.pathname;
+    const authToken = request.cookies.get(TOKEN_KEY);
 
     const publicRoute = publicRoutes.find(route => {
         if (typeof route.path === 'string') {
             return route.path === path;
         } else {
-            return route.path.test(path); // Verifica se a rota corresponde ao padrão dinâmico
+            return route.path.test(path);
         }
-    })
-
-    const authToken = request.cookies.get('token')
-
-    if (!authToken && publicRoute) {
-        return NextResponse.next()
-    }
+    });
 
     if (!authToken && !publicRoute) {
-        const redirectUrl = request.nextUrl.clone()
-
-        redirectUrl.pathname = REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE
-
-        return NextResponse.redirect(redirectUrl)
-
+        const redirectUrl = request.nextUrl.clone();
+        redirectUrl.pathname = REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE;
+        return NextResponse.redirect(redirectUrl);
     }
+
     if (authToken && publicRoute && publicRoute.whenAuthenticated === 'redirect') {
-        const redirectUrl = request.nextUrl.clone()
-
-        redirectUrl.pathname = '/'
-
-        return NextResponse.redirect(redirectUrl)
+        const redirectUrl = request.nextUrl.clone();
+        redirectUrl.pathname = REDIRECT_WHEN_AUTHENTICATED_ROUTE;
+        return NextResponse.redirect(redirectUrl);
     }
 
-    if (authToken && !publicRoute) {
-        // validar se o jwt está expirado
-        // se sim, remover o token
-        return NextResponse.next()
-    }
-    return NextResponse.next()
+    return NextResponse.next();
 }
 
 export const config: MiddlewareConfig = {
     matcher: [
-        /*
-         * Match all request paths except for the ones starting with:
-         * - api (API routes)
-         * - _next/static (static files)
-         * - _next/image (image optimization files)
-         * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-         */
         '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
     ],
-}
+};
