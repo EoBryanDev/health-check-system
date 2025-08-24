ddb10df04fd3f58d8897ecd620e63c3f14477dbc
Bryan-Galaxy-WSL-Ubuntu <eobryandev@gmail.com>
2025-08-24 12:07:45 -0300

    feat: adjusting structure

--- front/src/app/(private)/dashboard/page.tsx
+++ front/src/app/(private)/dashboard/page.tsx
@@ -1,11 +1,14 @@
-import { DashboardMain } from "@/pages/DashboardMain";
+// front/src/app/dashboard/page.tsx
+import { LoadingState } from '@/components/loading';
+import { DashboardMain } from '@/pages/DashboardMain';
+import { Suspense } from 'react';
 
-
-export default function Dashboard() {
-  return (
-      <main className="flex-1">
-        <DashboardMain />
-      </main>
-  )  
-  ;
+export default function DashboardPage() {
+    return (
+        <main className="flex-1">
+            <Suspense fallback={<LoadingState />}>
+                <DashboardMain />
+            </Suspense>
+        </main>
+    );
 }
